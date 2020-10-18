2a91cb8f13ab3c6d9d20264cf5ef0d8329d5f28f
L-M-K-B <Laura.M.K.Benz@gmail.com>
2020-10-18 15:39:25 +0200

    refactor: clean up and create class

--- thesaurus/main_db.py
+++ thesaurus/main_db.py
@@ -14,62 +14,62 @@ con = mysql.connector.connect(
 cursor = con.cursor()
 
 
-def handle_query(search_term):
-    query = cursor.execute(f"SELECT Definition FROM Dictionary WHERE Expression = '{search_term}'")
-    definitions = cursor.fetchall()
+class WordFinder:
+    def __init__(self):
+        query_alt = cursor.execute("SELECT Expression FROM Dictionary")
+        expressions = cursor.fetchall()
 
-    return definitions
+        self.exp_list = self.clean_expressions(expressions)
+
+    def clean_expressions(self, exp):
+        clean_exp = []
+
+        for e in exp:
+            clean_exp.append(e[0])
+
+        return clean_exp
+
+    def handle_query(self, search_term):
+        query = cursor.execute(f"SELECT Definition FROM Dictionary WHERE Expression = '{search_term}'")
+        definitions = cursor.fetchall()
+
+        return definitions
+
+    def handle_results(self, results):
+        for r in results:
+            print(r[0])
+
+    def handle_no_results(self):
+        if closest_matches := difflib.get_close_matches(word, self.exp_list):
+            # closest_matches always has 3 suggestions
+            self.handle_closest_match(closest_matches[0])
+
+        else:
+            print("This word does not exist. Please check again.")
+
+    def handle_closest_match(self, match):
+        decision = input(
+            f"Did you mean {match}? Type Y for yes or N for no.")
+
+        if decision == 'Y':
+            alt = self.handle_query(match)
+            self.handle_results(alt)
+
+        else:
+            return None
 
 
-def handle_results(results):
-    for r in results:
-        print(r[0])
-
-
-def handle_no_results():
-    query_alt = cursor.execute("SELECT Expression FROM Dictionary")
-    expressions = cursor.fetchall()
-
-    exp_list = clean_expressions(expressions)
-
-    if closest_matches := difflib.get_close_matches(word, exp_list):
-        # closest_matches always has 3 suggestions
-        handle_closest_match(closest_matches[0])
-
-    else:
-        print("This word does not exist. Please check again.")
-
-
-def handle_closest_match(match):
-    decision = input(
-        f"Did you mean {match}? Type Y for yes or N for no.")
-
-    if decision == 'Y':
-        alt = handle_query(match)
-        handle_results(alt)
-
-    else:
-        return None
-
-
-def clean_expressions(exp):
-    clean_exp = []
-
-    for e in exp:
-        clean_exp.append(e[0])
-
-    return clean_exp
-
+word_finder = WordFinder()
 
 while word != END:
     word = input("Enter a word: ")
 
     if word != END:
-        result = handle_query(word)
+        result = word_finder.handle_query(word)
 
         if result:
-            handle_results(result)
+            word_finder.handle_results(result)
 
         else:
-            handle_no_results()
+            word_finder.handle_no_results()
 
