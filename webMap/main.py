91483e56c90cce148e9eea9d04275d7f688500d4
L-M-K-B <Laura.M.K.Benz@gmail.com>
2020-11-08 17:20:40 +0100

    feat: add a polygon layer for population

--- webMap/main.py
+++ webMap/main.py
@@ -12,21 +12,27 @@ elev = list(volcanoes.loc[:, "ELEV"])
 def color_elevation(elevation):
     if elevation < 1000:
         return "green"
-    elif 1000 <= elevation <3000:
+    elif 1000 <= elevation < 3000:
         return "orange"
     else:
         return "purple"
 
 
 # create a map of volcanoes
-map_volcanoes = folium.Map(location=[41.520164, -100.320301], zoom_start=5, tiles="Stamen Watercolor")
+map_volcanoes = folium.Map(location=[41.520164, -100.320301], zoom_start=5, tiles="Stamen Toner")
 
 # create a layer for markers and add markers
-markers_layer = folium.FeatureGroup(name="markers")
+layers = folium.FeatureGroup(name="markers")
 
 for lt, ln, n, e in zip(lat, lon, name, elev):
-    markers_layer.add_child(
-        folium.CircleMarker(location=(lt, ln), popup=f"Name: {n} Height: {e}m", color="black", fill=True, fill_color=color_elevation(e), fill_opacity=0.7))
+    layers.add_child(
+        folium.CircleMarker(location=(lt, ln), popup=f"Name: {n} Height: {e}m", color="black", fill=True,
+                            fill_color=color_elevation(e), fill_opacity=0.7))
 
-map_volcanoes.add_child(markers_layer)
+layers.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(), style_function=lambda x:
+{"fillColor": "green" if x["properties"]["POP2005"] < 10000000 else "orange" if 10000000 <= x["properties"][
+    "POP2005"] < 20000000 else "red" if 20000000 <= x["properties"][
+    "POP2005"] < 50000000 else "darkpurple"}))
+
+map_volcanoes.add_child(layers)
 map_volcanoes.save("MapVolcanoes.html")
