18bc0fa8fe4216b26cddc2a761d2ee9258d34cb2
L-M-K-B <Laura.M.K.Benz@gmail.com>
2020-11-17 22:07:57 +0100

    feat: complete backend

--- bookstore/backend.py
+++ bookstore/backend.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-def connect_to_db(sql):
+def connect(sql):
     conn = sqlite3.connect("bookstore.db")
     cur = conn.cursor()
     cur.execute(sql)
@@ -9,29 +9,45 @@ def connect_to_db(sql):
     conn.close()
 
 
+def connect_fetch(sql):
+    conn = sqlite3.connect("bookstore.db")
+    cur = conn.cursor()
+    cur.execute(sql)
+    all_books = cur.fetchall()
+    conn.close()
+    return all_books
+
+
 def create_table():
-    connect_to_db("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, author TEXT, isbn INTEGER)")
+    connect("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, "
+            "author TEXT, isbn INTEGER)")
 
 
 create_table()
 
 
 def insert(title, year, author, isbn):
-    connect_to_db(f"INSERT INTO books VALUES(NULL, '{title}', {year}, '{author}', {isbn})")
+    connect(f"INSERT INTO books VALUES(NULL, '{title}', {year}, '{author}', {isbn})")
 
 
 def delete(book_id):
-    connect_to_db(f"DELETE FROM books WHERE id={book_id}")
+    connect(f"DELETE FROM books WHERE id={book_id}")
 
 
-def update(book_id, title, author, year, isbn):
-    connect_to_db(f"UPDATE books SET year={year}, isbn={isbn}, author='{author}', title='{title}' WHERE id={book_id}")
+def update(book_id, title="", author="", year="", isbn=""):
+    if title:
+        connect(f"UPDATE books SET title='{title}' WHERE id={book_id}")
+    if author:
+        connect(f"UPDATE books SET author='{author}' WHERE id={book_id}")
+    if year:
+        connect(f"UPDATE books SET year='{year}' WHERE id={book_id}")
+    if isbn:
+        connect(f"UPDATE books SET isbn='{isbn}' WHERE id={book_id}")
 
 
 def view_all():
-    conn = sqlite3.connect("bookstore.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM books")
-    all_books = cur.fetchall()
-    conn.close()
-    return all_books
+    return connect_fetch("SELECT * FROM books")
+
+
+def search(title="", year="", author="", isbn=""):
+    return connect_fetch(f"SELECT * FROM books WHERE title='{title}' OR year='{year}' OR author='{author}' OR isbn='{isbn}'")
