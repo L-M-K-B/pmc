c4eaa665b72a893134f0a4cb76f18dd753b5ea8b
L-M-K-B <Laura.M.K.Benz@gmail.com>
2020-11-29 19:41:03 +0100

    feat: create basic structure of app

---
+++ feelGoodApp/main.py
@@ -0,0 +1,31 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Builder.load_file('design.kv')
+
+
+class LoginScreen(Screen):
+
+    def sign_up(self):
+        self.manager.current = 'sign_up_screen'
+
+
+class SignupScreen(Screen):
+
+    def sign_up(self):
+        print('Signed up in this app')
+
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+
+    def build(self):
+        return RootWidget()
+
+
+if __name__ == "__main__":
+    MainApp().run()
