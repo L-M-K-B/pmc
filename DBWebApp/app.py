360f853205a79da40a6331fab676969de3034d71
L-M-K-B <Laura.M.K.Benz@gmail.com>
2020-12-29 17:01:36 +0100

    feat: add data to db

--- DBWebApp/app.py
+++ DBWebApp/app.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import sys
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://laura@localhost:5432/db_web_app'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Heights(db.Model):
+    __tablename__ = "heights"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    height = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, email_, height_):
+        self.email = email_
+        self.height = height_
+
+
+# db.create_all()
 
 
 @app.route("/")
@@ -11,8 +30,18 @@ def home_page():
 @app.route("/success/", methods=['POST'])
 def success_page():
     if request.method == 'POST':
-        email = request.form("email_name")
-        height = request.form("height_name")
+        try:
+            email = request.form["email_name"]
+            height = request.form["height_name"]
+            entry = Heights(email, height)
+            db.session.add(entry)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            print(sys.exc_info())
+        finally:
+            db.session.close()
+
     return render_template("success.html")
 
 
